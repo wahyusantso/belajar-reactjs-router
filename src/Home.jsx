c52c9871bdaedc6fdec1737af31dbedb03f7d746
wahyusantso <wahyusantozo36@gmail.com>
2025-08-06 20:15:31 +0700

    feat(54): Use Navigate

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router"
+
 export default function Home() {
+
+    const navigate = useNavigate(); //menggunakan component hook untuk pindah halaman.
+
+    function handleClick() {
+        navigate({
+            pathname: "/data" //url yang dituju.
+        })
+    }
     return (
         <div>
             <h1>Home</h1>
             <p>Welcome the Dashboard page.</p>
+            <button onClick={handleClick}>Go to Data page</button>
         </div>
     )
 }
