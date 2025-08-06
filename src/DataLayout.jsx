3ae71845d6d7e9eaa9c97520016f35bf6196dbf2
wahyusantso <wahyusantozo36@gmail.com>
2025-08-06 20:06:29 +0700

    feat(53): Navigation

--- src/DataLayout.jsx
+++ src/DataLayout.jsx
@@ -1,10 +1,41 @@
-import { Outlet } from "react-router";
+import { Link, NavLink, Outlet } from "react-router";
+import "../public/style.css";
 
 export default function DataLayout() {
     return (
         <>
             <div>
                 <h1 style={{ color: "blue" }}>This is Header</h1>
+            </div>
+            <div>
+                <ul style={{ display: "flex", gap: "40px" }}>
+                    {
+                        /*
+                        - menggunakan component Link, sebagai pengganti element <a> Anchor.
+                        - Anchor element dapat menyebabkan reload ulang saat berpindah halaman.
+                         */
+                    }
+                    {/* <li><Link to="/data/products">Products</Link></li>
+                    <li><Link to="/data/customers">Costumers</Link></li>
+                    <li><Link to="/data/sellers">Sellers</Link></li> */}
+                    {/* implementasi parameter pada component Link */}
+                    {/* <li><Link to={{ 
+                        pathname: "/data/products",
+                        search: "?category=weapon", //as params
+                        hash: "#top"
+                     }}>Products With Params</Link></li> */}
+
+                    
+                    { /* menggunakan NavLink untuk mengetahui posisi navigasi yang dipilih */}
+                    <li><NavLink to="/data/products">Products</NavLink></li>
+                    <li><NavLink to="/data/customers">Costumers</NavLink></li>
+                    <li><NavLink to="/data/sellers">Sellers</NavLink></li>
+                    <li><NavLink to={{ 
+                        pathname: "/data/products",
+                        search: "?category=weapon", //as params
+                        hash: "#top"
+                     }}>Products With Params</NavLink></li>
+                </ul>
             </div>
             <div>
                 <Outlet/> {/* akan menambung child component */}
